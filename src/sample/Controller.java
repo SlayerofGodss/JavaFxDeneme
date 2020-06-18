f2ebd69b74adf08d728dc74812815388c020a837
DİYAR <diyarbjk44@gmail.coo>
2020-06-18 13:31:19 +0300

    Text Field with Scene Builder

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,14 +1,13 @@
 package sample;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import javafx.scene.control.TextField;
 public class Controller {
-@FXML
-    Label lbl;
-public void fontDegistir(){
-    lbl.setFont(Font.font("Consolas",FontWeight.BOLD,25));
-}
+    @FXML
+    TextField txt;
+    public void yaz(){
+        txt.setText("Selamın Aleyküm Bremın");
+    }
+
 }
 
 
