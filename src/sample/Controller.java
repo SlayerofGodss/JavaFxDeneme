479611a867634bbfdb1ca98ad1d3bf2864bd5935
DİYAR <diyarbjk44@gmail.coo>
2020-06-19 12:48:50 +0300

    Numbers Only

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,16 @@
 package sample;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 public class Controller {
-    @FXML
-        TextField sayi1,sayi2,sonuc;
-        public void toplaEvent(){
-            sonuc.setText(String.valueOf(Integer.parseInt(sayi1.getText())+Integer.parseInt(sayi2.getText())));
-    }
+   @FXML
+    TextField txt;
+   @FXML
+    Label lbl;
+   public void GosterEvent(){
+       String k=txt.getText();
+       if (! k.matches("[0-9]*"))
+           lbl.setText("Sadece Sayı Girebilirsizniz");
+   }
 }
 
