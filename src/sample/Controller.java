8d636f44cb7a685fb8dbfe94995d4d414ca71468
DİYAR <diyarbjk44@gmail.coo>
2020-06-19 11:06:41 +0300

    KARAKTER!!!!!!!!

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,16 +1,16 @@
 package sample;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 public class Controller {
 @FXML
-    TextField txtOkul, txtBolum ,txtSonuc;
-public void onayEvent(){
-    txtSonuc.setText(txtOkul.getText()+" "+txtBolum.getText());
- }
-public void silEvent(){
-    txtOkul.clear();
-    txtBolum.clear();
-    txtSonuc.clear();
+    TextField txt;
+@FXML
+Label lbl;
+    public void bulEvent(){
+    int uzunluk;
+    uzunluk=txt.getLength();
+    lbl.setText("kelime uzunluğu:"+uzunluk+" karakter");
 }
 }
 
