c80afcb87133217bd232868ffec37ae8d54e323c
DİYAR <diyarbjk44@gmail.coo>
2020-06-19 12:39:57 +0300

    2.0

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,12 +3,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 public class Controller {
     @FXML
-    TextField sayi1,sayi2,sonuc;
-    public void toplaEvent(){
-        int a,b;
-        a=Integer.valueOf(sayi1.getText());
-        b=Integer.valueOf(sayi2.getText());
-        sonuc.setText(String.valueOf(a+b));
+        TextField sayi1,sayi2,sonuc;
+        public void toplaEvent(){
+            sonuc.setText(String.valueOf(Integer.parseInt(sayi1.getText())+Integer.parseInt(sayi2.getText())));
     }
 }
 
